ba589341a5a6b92c232ab938ea90ab62970db2fe
anthony.b.chung <anthony.b.chung@gmail.com>
2024-05-16 11:58:47 +1000

    init

---
+++ app/src/main/java/com/example/toolbox/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.toolbox
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import dev.hotwire.turbo.activities.TurboActivity
+import dev.hotwire.turbo.delegates.TurboActivityDelegate
+
+
+class MainActivity : AppCompatActivity(), TurboActivity {
+    override lateinit var delegate: TurboActivityDelegate
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+        delegate = TurboActivityDelegate(this, R.id.main_nav_host)
+    }
+
+}
